d45c6b1134f21f7f3d3099e72db9c3a8fbcd33ad
YoannDev90 <yoanndev@outlook.fr>
2026-03-31 09:20:03 +0200

    feat: Add JSON dependency and improve logging functionality with ASCII support

--- src/client/kotlin/stackabletoolskotlin/StackableToolsKotlinClient.kt
+++ src/client/kotlin/stackabletoolskotlin/StackableToolsKotlinClient.kt
@@ -5,12 +5,6 @@ import stackabletoolskotlin.CustomLogger
 
 object StackableToolsKotlinClient : ClientModInitializer {
 	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		CustomLogger.info("Initialisation du client StackableToolsKotlin démarrée")
-		
-		// Ajout de logs pour vérifier le bon chargement du client
-		CustomLogger.info("Client StackableToolsKotlin chargé avec succès")
-		
-		CustomLogger.info("Initialisation du client StackableToolsKotlin terminée")
+		CustomLogger.info("Client StackableToolsKotlin initialisé")
 	}
 }
